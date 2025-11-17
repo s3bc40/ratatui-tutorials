9f76d310c9acc87ea7f909d16f8ea1d3b3de32bf
s3bc40 <s3bc40@proton.me>
2025-11-17 15:31:57 +0100

    feat: complete the main loop run app and event polling

--- ratatui-json-editor/src/main.rs
+++ ratatui-json-editor/src/main.rs
@@ -1,14 +1,18 @@
 use ratatui::Terminal;
-use ratatui::crossterm::event::{DisableMouseCapture, EnableMouseCapture};
+use ratatui::crossterm::event::{
+    self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode, KeyEventKind,
+};
 use ratatui::crossterm::execute;
 use ratatui::crossterm::terminal::{
     EnterAlternateScreen, LeaveAlternateScreen, disable_raw_mode, enable_raw_mode,
 };
-use ratatui::prelude::CrosstermBackend;
+use ratatui::prelude::{Backend, CrosstermBackend};
 use std::error::Error;
 use std::io;
 
-pub mod app;
+use crate::app::{CurrentScreen, CurrentlyEditing};
+
+mod app;
 
 fn main() -> Result<(), Box<dyn Error>> {
     // Setup terminal
@@ -41,4 +45,94 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
 
     Ok(())
+}
+
+fn run_app<B: Backend>(terminal: &mut Terminal<B>, app: &mut app::App) -> io::Result<bool> {
+    loop {
+        terminal.draw(|draw| ui(f, app))?;
+
+        // Event handling => Polling
+        // Could be a separate thread if needed
+        if let Event::Key(key) = event::read()? {
+            dbg!(key.code);
+            if key.kind == event::KeyEventKind::Release {
+                // Skip because we only care about key presses
+                continue;
+            }
+
+            // Main screen handling
+            match app.current_screen {
+                CurrentScreen::Main => match key.code {
+                    // Key code when in main screen
+                    KeyCode::Char('e') => {
+                        app.current_screen = CurrentScreen::Editing;
+                        app.currently_editing = Some(CurrentlyEditing::Key);
+                    }
+                    KeyCode::Char('q') => {
+                        app.current_screen = CurrentScreen::Exiting;
+                    }
+                    _ => {}
+                },
+                // Exiting screen handling
+                CurrentScreen::Exiting => match key.code {
+                    KeyCode::Char('y') => {
+                        return Ok(true);
+                    }
+                    KeyCode::Char('n') | KeyCode::Char('q') => {
+                        return Ok(false);
+                    }
+                    _ => {}
+                },
+                // Editing screen handling
+                CurrentScreen::Editing if key.kind == KeyEventKind::Press => match key.code {
+                    KeyCode::Enter => {
+                        if let Some(editing) = &app.currently_editing {
+                            match editing {
+                                CurrentlyEditing::Key => {
+                                    app.currently_editing = Some(CurrentlyEditing::Value);
+                                }
+                                CurrentlyEditing::Value => {
+                                    app.save_key_value();
+                                    app.current_screen = CurrentScreen::Main;
+                                }
+                            }
+                        }
+                    }
+                    KeyCode::Backspace => {
+                        if let Some(editing) = &app.currently_editing {
+                            match editing {
+                                CurrentlyEditing::Key => {
+                                    app.key_input.pop();
+                                }
+                                CurrentlyEditing::Value => {
+                                    app.value_input.pop();
+                                }
+                            }
+                        }
+                    }
+                    KeyCode::Esc => {
+                        app.current_screen = CurrentScreen::Main;
+                        app.currently_editing = None;
+                    }
+                    KeyCode::Tab => {
+                        app.toggle_editing();
+                    }
+                    KeyCode::Char(value) => {
+                        if let Some(editing) = &app.currently_editing {
+                            match editing {
+                                CurrentlyEditing::Key => {
+                                    app.key_input.push(value);
+                                }
+                                CurrentlyEditing::Value => {
+                                    app.value_input.push(value);
+                                }
+                            }
+                        }
+                    }
+                    _ => {}
+                },
+                _ => {}
+            }
+        }
+    }
 }
